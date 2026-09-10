f514734ad3f24a071557ac51c14b51bae9ff01a2
Lxgends <marcitis24@gmail.com>
2026-09-10 15:19:49 +0300

    Added get average function
    Added a new function that gets the students average

--- TestFolder/CodeFragment.py
+++ TestFolder/CodeFragment.py
@@ -6,3 +6,8 @@ def add_grade(students, name, grade):
     if name in students:
         students[name].append(grade)
     return students
+
+def get_average(students, name):
+    if name in students and students[name]:
+        return sum(students[name]) / len(students[name])
+    return 0

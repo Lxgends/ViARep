f5443715bd4b1a6158da9342e062f352b73743f2
Lxgends <marcitis24@gmail.com>
2026-09-10 15:50:04 +0300

    Added a log function
    Added a function that logs conversions

--- FirstLecture/Converter.py
+++ FirstLecture/Converter.py
@@ -9,3 +9,6 @@ def fahrenheit_to_celsius(f):
 
 def kelvin_to_celsius(k):
     return k - 273.15
+
+def log_conversion(value, unit_from, unit_to, result):
+    history.append(f"{value}{unit_from} -> {result:.2f}{unit_to}")

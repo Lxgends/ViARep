8a38e7409845b66239f92527add937903a09f285
Lxgends <marcitis24@gmail.com>
2026-09-10 15:09:00 +0300

    Added a Add Grade function
    Added a new function that adds a grade to the students

--- TestFolder/CodeFragment.py
+++ TestFolder/CodeFragment.py
@@ -1,3 +1,8 @@
 def add_student(students, name):
     students[name] = []
     return students
+
+def add_grade(students, name, grade):
+    if name in students:
+        students[name].append(grade)
+    return students
